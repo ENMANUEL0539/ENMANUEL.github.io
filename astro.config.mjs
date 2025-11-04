4644490b1fbbf7e2b476d5d537ee088ce698ba84
Enmanuel <118722277+ENMANUEL0539@users.noreply.github.com>
2025-11-04 02:02:57 -0600

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ENMANUEL.github.io',
+    site: 'https://enmanuel0539.github.io',
   integrations: [mdx(), sitemap()],
 
   vite: {
